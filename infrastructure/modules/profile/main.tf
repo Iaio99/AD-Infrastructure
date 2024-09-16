8dca935d66a88f35a487dcb265c91fb819d4ea7b
Iaio99 <giuliano.vallone@ikmail.com>
2024-09-16 17:02:42 +0200

    Rimossa creazione del progetto incus

--- infrastructure/modules/profile/main.tf
+++ infrastructure/modules/profile/main.tf
@@ -7,15 +7,6 @@ resource "incus_profile" "profile" {
   name = "ad"
 
   device {
-    type = "nic"
-    name = "game0"
-    
-    properties = {
-      network = "vulnbox0"
-    }
-  } 
-
-  device {
     type = "disk"
     name = "root"
 
