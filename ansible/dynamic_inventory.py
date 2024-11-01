e0a5f271df206623e16c597eae2f613cd9cdc9a5
Iaio99 <giuliano.vallone@ikmail.com>
2024-11-01 20:29:34 +0100

    Merge dei ruoli del cluster e di ad-platform. Aggiornati di conseguanza ansible.cfg e dynamic_inventory.py

--- ansible/dynamic_inventory.py
+++ ansible/dynamic_inventory.py
@@ -6,10 +6,17 @@ import sys
 
 def main():
     with open('../configs.json', 'r') as f:
-        teams = json.load(f)["teams"]
+        configs = json.load(f)
+        teams = configs["ad-platform"]["teams"]
+        player_number = configs["ad-platform"]["player_number"]
+        nodes = json.load(f)["incus-cluster"]["nodes"]
+        
 
     vulnboxes = ["nop-vulnbox"]
     vpns = []
+    hosts = []
+    ovn_hosts = []
+
 
     for t in teams:
         vulnboxes.append(t+"-vulnbox")
@@ -18,13 +25,19 @@ def main():
     inventory = {
         "vulnboxes": {
             "hosts": vulnboxes
+            "vars": {
+                "ansible_remote": remote
+                "ansible_connection": "community.general.incus",
+            }
         },
 
         "vpns": {
             "hosts": vpns,
             "vars": {
                 "endpoint_address": "192.168.142.111",
-                "vpn_players": 6
+                "vpn_players": player_number
+                "ansible_connection": "community.general.incus",
+                "ansible_remote": remote
             }
         },
 
@@ -47,16 +60,54 @@ def main():
                 "ctf_gameserver_submission_listen_host": "0.0.0.0",
                 "ctf_gameserver_submission_listen_ports": [8080],
                 "ctf_gameserver_db_user_vpnstatus": "gameserver_vpnstatus",
-                "ctf_gameserver_web_allowed_hosts": ["{{ ansible_fqdn }}", "192.168.142.111"]
-            }
-        },
-        "all": {
-            "vars": {
+                "ctf_gameserver_web_allowed_hosts": ["{{ ansible_fqdn }}", "192.168.142.111"],
                 "ansible_connection": "community.general.incus",
-                "ansible_remote": 'local'
+                "ansible_remote": remote
             }
         }
     }
+
+    for n in nodes.keys():
+        hosts.append(n)
+        ovn_hosts.append(nodes[n])
+
+    if len(nodes) == 2:
+        raise Exception "Cluster dimension cannot be 2"
+
+    if len(nodes) == 1:
+        inventory += {
+            "nodes": {
+                "hosts": hosts,
+                "ansible_connection": "ssh"
+            },
+
+            "ovn_hosts": {
+                "hosts": ovn_hosts,
+                "vars": {
+                    "server_1": ovn_hosts[0],
+                    "ansible_connection": "ssh"
+                }
+            },
+        }
+
+    else: 
+        inventory += {
+            "nodes": {
+                "hosts": hosts
+                "ansible_connection": "ssh",
+            },
+
+            "ovn_hosts": {
+                "hosts": ovn_hosts,
+                "vars": {
+                    "server_1": ovn_hosts[0],
+                    "server_2": ovn_hosts[1],
+                    "server_3": ovn_hosts[2],
+                    "ansible_connection": "ssh",
+
+                }
+            },
+        }
 
     print(json.dumps(inventory, indent=2))
 
